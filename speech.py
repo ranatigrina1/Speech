ef28bfca0955b7e6a712e067cd63390468dfdd45
ranatigrina1 <91794021+ranatigrina1@users.noreply.github.com>
2021-10-02 21:31:58 +0545

    added instagram

--- speech.py
+++ speech.py
@@ -42,5 +42,20 @@ if 'youtube' in r2.recognize_google(audio):
     except sr.RequestError as e:
         print('failed'.format(e))
 
+if 'instagram' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    url = 'https://www.instagram.com/'
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r2.listen(source)
+    try:
+        get = r2.recognize_google(audio)
+        print(get)
+        wb.get().open_new(url + get)
+    except sr.UnknownValueError:
+        print('error')
+    except sr.RequestError as e:
+        print('failed'.format(e))
+
 
  
